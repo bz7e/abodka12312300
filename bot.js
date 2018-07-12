a78ecaa52de312dd2fc02008482b681f95eae907
bz7e <35487547+bz7e@users.noreply.github.com>
2018-07-12 17:40:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag} !`);
-        client.user.setActivity("Pùlla is coming..",{type: 'WATCHING'});
+        client.user.setActivity("with my babe",{type: 'WATCHING'});
  
 });
 
