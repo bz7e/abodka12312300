a193cc899cda8706d74e40d08feea2741d3ad0ee
bz7e <35487547+bz7e@users.noreply.github.com>
2018-07-13 02:20:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
-        client.user.setActivity("with my babe",{type: 'WATCHING'});
- 
-});
+const devs = ['358890439753072642' , '' , '' , '' , 'id 5' , 'id 6' , 'id 7'];
+const adminprefix = ",";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'pl')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  }
+  });
 
 client.login(process.env.BOT_TOKEN);
